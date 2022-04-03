f2d144cf94d6bf7e5111c483af476c0baf35a909
Rahul-Verma <94426476+rvstarcaptain@users.noreply.github.com>
2022-04-03 17:57:11 +0530

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Headrer</h1>
+        <a href='#'> Model S</a>
+        <a href='#'> Model X</a>
+        <a href='#'> Model Y</a>
+        <a href='#'> Model 3</a>
+        <a href='#'> Model Solar roof</a>
+        <a href='#'> Model Solar pannel</a>
+    
+    </div>
+  )
+}
+
+
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section from './Section';
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <Section/>
+    </div>
+  )
+}
+
+
+
